3600b614831d117c4da3a1bdca9710742541c11a
Marek Grzejszczak <marek.grzejszczak@wp.pl>
2022-04-09 15:44:03 +0200

    [#MFD-16] Nie zmieniala się nazwa elementu

--- src/app/components/right-side/toolbox/properties/element-types/common/name-input/name-input.component.ts
+++ src/app/components/right-side/toolbox/properties/element-types/common/name-input/name-input.component.ts
@@ -7,12 +7,26 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class NameInputComponent {
 
-  @Input() name: string;
+  _name: string;
+  _initialValue: string;
+
+  @Input()
+  set name(value: string) {
+    this._name = value;
+    this._initialValue = value;
+  }
 
   @Output() nameChange = new EventEmitter<string>();
 
   @Output() save = new EventEmitter<void>();
 
   constructor() {
+  }
+
+  updateIfNeeded(): void {
+    if (this._name !== this._initialValue) {
+      this.nameChange.emit(this._name);
+      this.save.emit();
+    }
   }
 }
